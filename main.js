8fa046f7bd362a5c8d4085a1262ebe77d19b57e9
ngocquochuy26 <quochuytranngoc@gmail.com>
2024-07-04 11:27:12 +0700

    fix home

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ var swiper = new Swiper(".home", {
     spaceBetween: 30,
     centeredSlides: true,
 
-
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
