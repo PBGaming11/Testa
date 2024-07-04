b05e22698dfe4987d150e1b996fd56b6fad8d076
PBGaming11 <phanbaobeo789@gmail.com>
2024-07-04 11:21:01 +0700

    sa

---
+++ login.js
@@ -0,0 +1,18 @@
+document.getElementById('login').onclick = function(event) {
+    //tam dung bieu mau
+    event.preventDefault();
+    //lay du lieu tu input
+    var username = document.getElementById('name').value;
+    var password = document.getElementById('pass').value;
+    if(username == "admin" && password == "admin"){
+        alert('thanh cong');
+    }else{
+        alert('that bai');
+    }
+};
+document.getElementById('cancelButton').onclick = function() {
+    //xoa thong tin o input
+    document.getElementById('name').value = '';
+    document.getElementById('pass').value = '';
+
+};
