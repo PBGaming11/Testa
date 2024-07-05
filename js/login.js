d8dc1dcaec8f47a1c2efe0305f1f6a750fb760c0
huutientai <phamhuutientai@gmail.com>
2024-07-05 08:04:10 +0700

    fix cart login

--- js/login.js
+++ js/login.js
@@ -10,12 +10,7 @@ document.getElementById('login').onclick = function(event) {
         alert('that bai');
     }
 };
-document.getElementById('cancelButton').onclick = function() {
-    //xoa thong tin o input
-    document.getElementById('name').value = '';
-    document.getElementById('pass').value = '';
 
-};
 document.getElementById('signin').onclick = function() {
     //chuyen trang
     window.location.href = 'signin.html';
